b688a71b2803e6126ab47771df956d409f965a47
Alimkhan Akimzhan <71062409+alimkhanakimzhan@users.noreply.github.com>
2021-03-10 21:43:00 +0500

    Update Customer.java

--- src/com/company/entities/Customer.java
+++ src/com/company/entities/Customer.java
@@ -1,5 +1,15 @@
 package com.company.entities;
 
 public class Customer extends Person {
+    private int balance;
 
+    public Customer(){}
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
